c7592375a53ac111284fd22afd1a39e3a56dc6b9
Ted M. Young <tedyoung@gmail.com>
2021-07-26 09:33:19 -0700

    Home page test passes.

---
+++ src/test/java/com/jitterted/ebp/blackjack/WebIntegrationTest.java
@@ -0,0 +1,24 @@
+package com.jitterted.ebp.blackjack;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class WebIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getOfHomePageIsStatus200Ok() throws Exception {
+        mockMvc.perform(get("/index.html"))
+               .andExpect(status().isOk());
+    }
+    
+}
