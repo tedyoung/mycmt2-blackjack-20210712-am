a435441d935a9faaed1f4972a07c48a2d8a8f5e7
Ted M. Young <tedyoung@gmail.com>
2021-07-26 10:11:34 -0700

    startGame does initial deal.
    POST to /startGame does a 3xx redirect

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -1,14 +1,24 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
+import com.jitterted.ebp.blackjack.domain.Game;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlackjackController {
 
+    private final Game game;
+
+    @Autowired
+    public BlackjackController(Game game) {
+        this.game = game;
+    }
+
     @PostMapping("/start-game")
     public String startGame() {
-        return "blackjack";
+        game.initialDeal();
+        return "redirect:/";
     }
 
 }
