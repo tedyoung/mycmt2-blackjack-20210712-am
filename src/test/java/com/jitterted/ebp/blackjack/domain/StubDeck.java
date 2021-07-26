fb03357ac5f9ead793eafbdead07d0172a5c32e1
Ted M. Young <tedyoung@gmail.com>
2021-07-26 10:32:08 -0700

    Game view (/game) endpoint returns cards as a view and is displayed in the template.

--- src/test/java/com/jitterted/ebp/blackjack/domain/StubDeck.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/StubDeck.java
@@ -4,20 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
-class StubDeck extends Deck {
-  private static final Suit DUMMY_SUIT = Suit.HEARTS;
-  private final ListIterator<Card> iterator;
+public class StubDeck extends Deck {
+    private static final Suit DUMMY_SUIT = Suit.HEARTS;
+    private final ListIterator<Card> iterator;
 
-  public StubDeck(Rank... ranks) {
-    List<Card> cards = new ArrayList<>();
-    for (Rank rank : ranks) {
-      cards.add(new Card(DUMMY_SUIT, rank));
+    public StubDeck(Rank... ranks) {
+        List<Card> cards = new ArrayList<>();
+        for (Rank rank : ranks) {
+            cards.add(new Card(DUMMY_SUIT, rank));
+        }
+        this.iterator = cards.listIterator();
     }
-    this.iterator = cards.listIterator();
-  }
 
-  @Override
-  public Card draw() {
-    return iterator.next();
-  }
+    public StubDeck(List<Card> cards) {
+        this.iterator = cards.listIterator();
+    }
+
+    @Override
+    public Card draw() {
+        return iterator.next();
+    }
 }
